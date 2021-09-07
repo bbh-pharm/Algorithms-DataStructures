9852bfcec4dd278b2d49f5c09d4e52d5ce6eebe6
bbh-pharm <87463552+bbh-pharm@users.noreply.github.com>
2021-09-07 09:50:36 +0900

    create BOJ solved problem (11021)

---
+++ BOJ/11021번-A+B -7.py
@@ -0,0 +1,9 @@
+#  (0 < A, B < 10)
+T = int(input())
+
+for n in range(T):
+  num_list = [int(num) for num in input().split()]
+  A = num_list[0]; B = num_list[1]
+  assert 0 < A < 10 and 0 < B < 10
+
+  print("Case #{}: {}".format(n+1, A+B))
